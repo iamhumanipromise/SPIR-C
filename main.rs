78893387b2d2874ced1bff761b10c229e6ccc4a9
iamhumanipromise <105223045+iamhumanipromise@users.noreply.github.com>
2023-05-09 23:51:48 -0700

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,45 +1,22 @@
-use std::fs;
-use std::io;
-use std::path::PathBuf;
-use clap::{Arg, App};
+use std::fs::File;
+use std::io::prelude::*;
 
-// Define our CLI arguments
-fn define_cli() -> App<'static, 'static> {
-    App::new("SPIR-C")
-        .version("0.1.0")
-        .author("Your Name <yourname@example.com>")
-        .about("Compiles GLSL to SPIR-V bytecode")
-        .arg(Arg::with_name("input")
-             .short("i")
-             .long("input")
-             .value_name("FILE")
-             .help("Input GLSL file to compile")
-             .takes_value(true)
-             .required(true))
-        .arg(Arg::with_name("output")
-             .short("o")
-             .long("output")
-             .value_name("FILE")
-             .help("Output SPIR-V file")
-             .takes_value(true)
-             .required(true))
-}
+mod vertex_flow;
+mod shader_ripper;
+mod spirv_optimizer;
+mod glsl_to_spirv;
 
-fn main() -> io::Result<()> {
-    let matches = define_cli().get_matches();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = File::open("input.glsl")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
 
-    // Get input and output file paths
-    let input_path = PathBuf::from(matches.value_of("input").unwrap());
-    let output_path = PathBuf::from(matches.value_of("output").unwrap());
+    let vertex_flow = vertex_flow::VertexFlow::new(&contents)?;
+    let spirv = glsl_to_spirv::compile(&contents)?;
+    let spirv_optimized = spirv_optimizer::optimize(&spirv)?;
+    let spirv_as_bytes = spirv_optimized.as_binary_u8();
 
-    // Read input file
-    let input_str = fs::read_to_string(&input_path)?;
-
-    // Call the SPIR-C compiler
-    let spirv_code = spir_c::compile(&input_str);
-
-    // Write output file
-    fs::write(output_path, spirv_code)?;
+    shader_ripper::rip_shader(&spirv_as_bytes)?;
 
     Ok(())
 }
